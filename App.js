1920367677df6b881cdc45046717c43762664d90
rizwanatta <rizwanatta@protonmail.com>
2024-06-22 13:50:50 +0500

    imgs, infile styled login

--- App.js
+++ App.js
@@ -1,66 +1,87 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, Buttong, Image, Text, TextInput, View } from "react-native";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 import appIcon from "./assets/icon.png";
 
 export default function App() {
   return (
-    <View style={{ height: "100%", width: "100%" }}>
+    <View style={styles.container}>
       <StatusBar style="light" />
 
-      <View
-        style={{
-          height: "30%",
-          width: "100%",
-          backgroundColor: "pink",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Image style={{ height: 100, width: 100 }} source={appIcon} />
-      </View>
-
-      <View
-        style={{
-          height: "40%",
-          width: "100%",
-          padding: 10,
-        }}
-      >
-        <TextInput
-          placeholder="Enter your Email"
-          style={{
-            borderWidth: 1,
-            padding: 10,
-            marginBottom: 10,
-            marginTop: 10,
+      <View style={styles.topContainer}>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: "https://cdn.pixabay.com/photo/2023/08/08/10/50/hot-wheels-8177051_1280.jpg",
           }}
         />
-        <TextInput
-          placeholder="Enter your Password"
-          style={{ borderWidth: 1, padding: 10, marginTop: 10 }}
-        />
-
-        <View style={{ alignSelf: "flex-end", paddingVertical: 10 }}>
-          <Text
-            style={{
-              textDecorationLine: "underline",
-              color: "blue",
-              fontWeight: "bold",
-              letterSpacing: 2,
-              textTransform: "uppercase",
-            }}
-          >
-            Forgot Password?
-          </Text>
-        </View>
-
-        <View style={{ marginTop: 10 }}>
-          <Button title={"Login"} />
-        </View>
       </View>
 
-      <View style={{ height: "30%", width: "100%" }}></View>
+      <View style={styles.formCon}>
+        <TextInput placeholder="Enter your Email" style={styles.inputCon} />
+        <TextInput placeholder="Enter your Password" style={styles.inputCon} />
+
+        <View style={styles.forgotPassCon}>
+          <Text style={styles.forgotPassText}>Forgot Password?</Text>
+        </View>
+
+        <View style={styles.buttonCon}>
+          <Button title={"Login"} color="pink" />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "orange",
+  },
+
+  topContainer: {
+    height: "30%",
+    width: "100%",
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  logo: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+  },
+
+  formCon: {
+    height: "40%",
+    width: "100%",
+    padding: 10,
+  },
+
+  inputCon: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+
+  forgotPassCon: {
+    alignSelf: "flex-end",
+    paddingVertical: 10,
+  },
+
+  forgotPassText: {
+    textDecorationLine: "underline",
+    color: "blue",
+    fontWeight: "bold",
+    letterSpacing: 2,
+    textTransform: "uppercase",
+  },
+  buttonCon: {
+    marginTop: 10,
+    width: 200,
+    alignSelf: "center",
+  },
+});
